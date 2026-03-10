50d71b5218b16f3e9e4ad4dc75083e313b62d688
jonibekmurtazoqulov <murtazoqulovjonik@gmail.com>
2026-03-10 18:24:17 +0500

    Navbar tayyor bo'ldi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,27 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Navbar() {
+    const pathname = usePathname();
+
+    const navbarMenu = [
+        { name: "Blog", path: "/blog" },
+        { name: "About", path: "/about" },
+    ];
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+        <div className="navbar-container">
+            <img src="../nav-logo.png" alt="no Img" className="nav-logo"/>
+            <div className="nav-link">
+                {navbarMenu.map((item, idx) => (
+                    <Link href={item.path} key={idx} className={`${ pathname === item.path ? "text-[#7C4EE4]" : ''}`}>{item.name}</Link>
+                ))}
+                <img src="../nav-search.png" alt="no Img" style={{ width: '23px', height: '23px', objectFit: 'contain' }}/>
+                <Link className="nav-contact-link" href={'/contact'}>Contact Us</Link>
+            </div>
+        </div>
+    </div>
   )
 }
